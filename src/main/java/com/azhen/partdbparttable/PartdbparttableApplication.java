faa67296afadc019ccb3f515605dc48ba66546db
Azhen <az2960426127@gmail.com>
2018-08-06 13:04:48 +0800

    Initial commit

---
+++ src/main/java/com/azhen/partdbparttable/PartdbparttableApplication.java
@@ -0,0 +1,12 @@
+package com.azhen.partdbparttable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartdbparttableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartdbparttableApplication.class, args);
+	}
+}
